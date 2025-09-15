84027ad488ed5fde3a5474bcd4cf3c333eeb8fe6
patriciapossarii <patricia.mag.carvalho@gmail.com>
2025-09-15 20:52:15 -0300

    Mudei a lista de nomes de usuarios para tornarem-se objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,4 +1,24 @@
-const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+//const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+
+
+const usuarios = [
+    {
+        nome: 'Mateus',
+        email: 'mateus@gospel.com'
+    },
+    {
+        nome: 'Marcos',
+        email: 'marcos@gospel.com'
+    },
+    {
+        nome: 'Lucas',
+        email: 'lucass@gospel.com'
+    },
+    {
+        nome: 'João',
+        email: 'joaao@gospel.com'
+    }
+]
 
 function retornarUsuarios() {
     return usuarios;
